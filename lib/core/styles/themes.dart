c62510316b82821034ca9e641665615f562c3f07
Fares Eed <eedf4730@gmail.com>
2026-03-05 23:52:41 +0200

    feat: integrate Poppins font and establish theme/text style utilities

---
+++ lib/core/styles/themes.dart
@@ -0,0 +1,6 @@
+import 'package:finwise/core/constants/app_fonts.dart';
+import 'package:flutter/material.dart';
+
+abstract class AppThemes {
+  static ThemeData get lightTheme => ThemeData(fontFamily: AppFonts.poppins);
+}
