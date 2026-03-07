dd30891a03d38e124cc96e395dc728b16fc95c7e
Eslam-Egy <eslamahmed2004.10@gmail.com>
2026-03-07 07:41:51 +0200

    add assets constants

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,10 +1,52 @@
 class AppAssets {
   // icons
-static const analysisSvg="assets/icons/Analysis.svg";
+  static const String analysis = "assets/icons/Analysis.svg";
+  static const String arrowDown = "assets/icons/arrow-down.svg";
+  static const String botInstagram = "assets/icons/Bot-Instagram.svg";
+  static const String botWhatsapp = "assets/icons/Bot-Whatsapp.svg";
+  static const String calender = "assets/icons/Calender.svg";
+  static const String cam = "assets/icons/cam.svg";
+  static const String car = "assets/icons/Car.svg";
+  static const String category = "assets/icons/Category.svg";
+  static const String chatCam = "assets/icons/chat-cam.svg";
+  static const String chatMic = "assets/icons/chat-mic.svg";
+  static const String chatShare = "assets/icons/chat-share.svg";
+  static const String checkProgress = "assets/icons/Check Progress.svg";
+  static const String check = "assets/icons/check.svg";
+  static const String dollar = "assets/icons/Dollar.svg";
+  static const String entertainment = "assets/icons/Entertainment.svg";
+  static const String expenseArrow = "assets/icons/Expense-arrow.svg";
+  static const String expense = "assets/icons/Expense.svg";
+  static const String eye = "assets/icons/Eye-Pass-close.svg";
+  static const String facebook = "assets/icons/Facebook.svg";
+  static const String fingerprint = "assets/icons/fingerPrint.svg";
+  static const String finwiseLogo = "assets/icons/finwiseLogo.svg";
+  static const String food = "assets/icons/Food.svg";
+  static const String gift = "assets/icons/Gift.svg";
+  static const String google = "assets/icons/Google.svg";
+  static const String groceries = "assets/icons/Groceries.svg";
+  static const String help = "assets/icons/help.svg";
+  static const String home = "assets/icons/Home.svg";
+  static const String income = "assets/icons/Income.svg";
+  static const String medicines = "assets/icons/Medicines.svg";
+  static const String newHome = "assets/icons/NewHome.svg";
+  static const String notification = "assets/icons/notification.svg";
+  static const String passSettings = "assets/icons/passSettings.svg";
+  static const String profile = "assets/icons/Profile.svg";
+  static const String rent = "assets/icons/Rent.svg";
+  static const String salary = "assets/icons/Salary.svg";
+  static const String saving = "assets/icons/Saving.svg";
+  static const String search = "assets/icons/search.svg";
+  static const String security = "assets/icons/Security.svg";
+  static const String settings = "assets/icons/Settings.svg";
+  static const String star = "assets/icons/Star.svg";
+  static const String transactions = "assets/icons/Transactions.svg";
+  static const String transport = "assets/icons/Transport.svg";
+  static const String travel = "assets/icons/Travel.svg";
+  static const String website = "assets/icons/Website.svg";
+  static const String wedding = "assets/icons/Wedding.svg";
 
-
-// imgs
-static const controlMoney = "assets/imgs/controlMoney.svg";
-static const expenseManager = "assets/imgs/expenseManager.svg";
-
+  // imgs
+  static const String controlMoney = "assets/imgs/controlMoney.svg";
+  static const String expenseManager = "assets/imgs/expenseManager.svg";
 }
