3c21ac5f4ef97b3b6df72e5d8e7f1fc452243d5e
SH-AHD <shahdsameh.fcis@gmail.com>
2026-03-25 01:03:15 +0200

    - Fix Scrolling issue

--- lib/core/widgets/my_body_view.dart
+++ lib/core/widgets/my_body_view.dart
@@ -11,40 +11,33 @@ class MyBodyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-  child: Column(
-          children: [
-            if (topSection != null) ...[
-              Padding(
+      body: Column(
+        children: [
+          if (topSection != null) ...[
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 37.0,
+                vertical: 20,
+              ),
+                child: topSection,
+            ),
+          ] else
+            const Gap(20),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: AppColors.background,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(70.0)),
+              ),
+              child: Padding(
                 padding: const EdgeInsets.symmetric(
                   horizontal: 37.0,
                   vertical: 20,
                 ),
-                  child: topSection,
+                child: bottomSection,
               ),
-            ] else
-              const Gap(20),],
-              ),
-          ),
-          SliverFillRemaining(
-hasScrollBody: false,
-      child: Container(
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  color: AppColors.background,
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(70.0)),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 37.0,
-                    vertical: 20,
-                  ),
-                  child: bottomSection,
-                ),
-              ),
-       
+            ),
           ),
         ],
       ),
--- lib/features/notification/pages/notification_screen.dart
+++ lib/features/notification/pages/notification_screen.dart
@@ -1,7 +1,6 @@
 import 'package:finwise/core/constants/app_assets.dart';
 import 'package:finwise/core/constants/app_colors.dart';
 import 'package:finwise/core/styles/text_styles.dart';
-import 'package:finwise/core/widgets/custom_svg_picture.dart';
 import 'package:finwise/core/widgets/default_app_bar.dart';
 import 'package:finwise/core/widgets/my_body_view.dart';
 import 'package:finwise/features/notification/data/notify_lists.dart';
@@ -18,14 +17,18 @@ class NotificationScreen extends StatelessWidget {
       appBar: DefaultAppBar(title: "Notification"),
       body: MyBodyView(
         
-        bottomSection: Column(
-          children: [
-            CompleteNotifySection(sectionName: "Today",notifyList: todaynotifyList,),
-            CompleteNotifySection(sectionName: "Yesterday",notifyList: yesterdaynotifyList,),
-            CompleteNotifySection(sectionName: "This Weekend",notifyList: thisWeekendnotifyList,),
-          
-          
-          ],
+        bottomSection: SingleChildScrollView(
+          child: Column(
+            children: [
+              CompleteNotifySection(sectionName: "Today",notifyList: todaynotifyList,),
+              Gap(25),
+              CompleteNotifySection(sectionName: "Yesterday",notifyList: yesterdaynotifyList,),
+                 Gap(25),
+              CompleteNotifySection(sectionName: "This Weekend",notifyList: thisWeekendnotifyList,),
+            
+            
+            ],
+          ),
         ),
 
 
@@ -35,98 +38,42 @@ class NotificationScreen extends StatelessWidget {
   }
 }
 
-// class CompleteNotifySection extends StatelessWidget {
-//   const CompleteNotifySection({
-//     super.key, required this.sectionName, required this.notifyList,
-//   });
-// final String sectionName;
-// final List<NotifyModel> notifyList;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       crossAxisAlignment: CrossAxisAlignment.start,
-//       children: [
-//         Text(sectionName,style: TextStyles.caption1_14.copyWith(fontWeight: FontWeight.w400),),
-//      Gap(7),
-    
-//     ListView.separated(
-//       physics: NeverScrollableScrollPhysics(),
-//       itemBuilder: (context,index){
-//         NotifyModel model=notifyList[index];
-//     return NotificationElement(iconPath: model.iconPath??AppAssets.notification,
-//     title: model.title??"",
-//     subTitle:model.subTitle??"",
-//     date: model.date??"",
-//     transactionDetails:model.transactionDetails
-//     );
-//       }, separatorBuilder: (context,index){
-//     return Column(
-//       children: [
-//     Gap(10),
-//         Container(
-//                     height: 1,
-//                     width: MediaQuery.of(context).size.width,
-//                     margin: const EdgeInsets.symmetric(vertical: 15),
-//                     color: AppColors.oceanBlueButton,
-//                   ),
-//       ],
-//     );
-//       }
-//       , itemCount: notifyList.length)
-//        ],
-//     );
-//   }
-// }
-
-
 class CompleteNotifySection extends StatelessWidget {
   const CompleteNotifySection({
     super.key, required this.sectionName, required this.notifyList,
   });
-  final String sectionName;
-  final List<NotifyModel> notifyList;
-
+final String sectionName;
+final List<NotifyModel> notifyList;
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(sectionName, style: TextStyles.caption1_14.copyWith(fontWeight: FontWeight.w400)),
-        const Gap(7),
-        
-        ...notifyList.asMap().entries.map((entry) {
-          int index = entry.key;
-          NotifyModel model = entry.value;
-
-          return Column(
-            children: [
-              NotificationElement(
-                iconPath: model.iconPath ?? AppAssets.notification,
-                title: model.title ?? "",
-                subTitle: model.subTitle ?? "",
-                date: model.date ?? "",
-                transactionDetails: model.transactionDetails,
-              ),
-                
-              if (index != notifyList.length - 1)
-                Container(
+        Text(sectionName,style: TextStyles.caption1_14.copyWith(fontWeight: FontWeight.w400),),
+     Gap(7),
+    
+    ListView.separated(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemBuilder: (context,index){
+        NotifyModel model=notifyList[index];
+    return NotificationElement(iconPath: model.iconPath??AppAssets.notification,
+    title: model.title??"",
+    subTitle:model.subTitle??"",
+    date: model.date??"",
+    transactionDetails:model.transactionDetails
+    );
+      }, separatorBuilder: (context,index){
+    return   Container(
                   height: 1,
                   width: double.infinity,
                   margin: const EdgeInsets.only(top: 15,bottom: 25),
                   color: AppColors.oceanBlueButton.withValues(alpha: 0.5),
-                ),
-            ],
-          );
-        }),
-        const Gap(25), 
-      ],
+                );
+      }
+      , itemCount: notifyList.length)
+       ],
     );
   }
 }
-
-
-
-
-
-
 
