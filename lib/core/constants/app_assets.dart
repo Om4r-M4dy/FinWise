60abd719ac4266c59b7cc755e4c6003b4d23bcd4
Fares Eed <eedf4730@gmail.com>
2026-03-24 14:56:06 +0200

    feet/ProfileScreen

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -45,7 +45,7 @@ class AppAssets {
   static const String saving = "assets/icons/Saving.svg";
   static const String search = "assets/icons/search.svg";
   static const String security = "assets/icons/Security.svg";
-  static const String settings = "assets/icons/Settings.svg";
+  static const String setting = "assets/icons/Setting.svg";
   static const String star = "assets/icons/Star.svg";
   static const String transactions = "assets/icons/Transactions.svg";
   static const String transport = "assets/icons/Transport.svg";
@@ -56,10 +56,13 @@ class AppAssets {
   static const String botInstagram = "assets/icons/bot_instagram.svg";
   static const String botSupport = "assets/icons/bot_support.svg";
   static const String botWhatsapp = "assets/icons/bot_whatsapp.svg";
+  static const String help = "assets/icons/help.svg";
+  static const String legout = "assets/icons/logout.svg";
 
   // imgs
   static const String controlMoney = "assets/imgs/controlMoney.png";
   static const String expenseManager = "assets/imgs/expenseManager.png";
+  static const String profileImage = "assets/imgs/profile_image.png";
 
   //lottie
   static const String logoJson = "assets/lottie/logo.json";
