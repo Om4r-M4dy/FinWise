035c3ab673c8febfa4cd0b5f86e911fa5da04b2e
Fares Eed <eedf4730@gmail.com>
2026-06-29 16:30:47 +0300

    .

--- lib/core/styles/text_styles.dart
+++ lib/core/styles/text_styles.dart
@@ -5,5 +5,5 @@ class TextStyles {
   static const TextStyle headlineLarge = TextStyle(fontSize: 24, fontWeight: FontWeight.w600);
   static const TextStyle bodyLarge      = TextStyle(fontSize: 18, fontWeight: FontWeight.w500);
   static const TextStyle bodyMedium     = TextStyle(fontSize: 16, fontWeight: FontWeight.w400);
-  static const TextStyle bodySmall      = TextStyle(fontSize:  12, fontWeight: FontWeight.w400);
+  static const TextStyle bodySmall      = TextStyle(fontSize: 12, fontWeight: FontWeight.w400);
 }
